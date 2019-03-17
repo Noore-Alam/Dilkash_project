821550ea74612e19006632217acd13aa408ad34c
Noore-Alam <mdnoore.alam78@gmail.com>
2019-03-17 14:35:49 +0530

    css & img & js & imdex.html

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-new WOW().init();
+$(function() {
+    new WOW().init();
+});
+
+$(function() {
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 50) {
+            $("header").addClass("header_bg");
+        } else {
+            $("header").removeClass("header_bg");
+        }
+    })
+})
